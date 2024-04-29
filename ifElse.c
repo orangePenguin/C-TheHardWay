145852d19a4648dfdf12742baa1415a7e407894d
Mayank Pawar <mayankpawar2612@gmail.com>
2024-04-29 10:00:17 +0530

    final commit

--- ifElse.c
+++ ifElse.c
@@ -5,10 +5,10 @@ int main(int argc, char*argv[])
     int i = 0;
     if (argc == 1) {
         printf("You only have one argument!\n");
-    } else if (argc > 1 && argc < 4) {
+    } else if (argc > 1 && argc < 5) {
         printf("Here are your arguments:\n");
 
-        for(i = 0; i < argc; i++) {
+        for(i = 1; i < argc; i++) {
             printf("%s ", argv[i]);
         }
         printf("\n");

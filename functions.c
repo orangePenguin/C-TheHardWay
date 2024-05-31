177b36c635852be4d514c1135c73daffa3c56a81
Mayank Pawar <mayankpawar2612@gmail.com>
2024-05-31 21:18:16 +0530

    final commit

--- functions.c
+++ functions.c
@@ -7,30 +7,30 @@ void print_letters(char arg[]);
 
 void print_arguments(int argc, char*argv[])
 {
-  for (int i = 0; i < argc; i++) {
-    print_letters(argv[i]);
-  }
+    for (int i = 0; i < argc; i++) {
+        print_letters(argv[i]);
+    }
 }
 
 void print_letters(char arg[])
 {
-  for (int i = 0; arg[i] != '\0'; i++) {
-    char ch = arg[i];
+    for (int i = 0; arg[i] != '\0'; i++) {
+        char ch = arg[i];
 
-    if (can_print_it(ch)) {
-      printf("'%c' == %d ", ch, ch);
+        if(can_print_it(ch)){
+            printf("'%c' == %d ", ch, ch);
+        }
     }
-  }
-  printf("\n");
+    printf("\n");
 }
 
 int can_print_it(char ch)
 {
-  return isalpha(ch) || isblank(ch);
+    return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[])
 {
-  print_arguments(argc, argv);
-  return 0;
+    print_arguments(argc, argv);
+    return 0;
 }
--- pointers.c
+++ pointers.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 
-int int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int ages[] = {23, 43, 12, 89, 2};
-  char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa"};
+    int ages[] = {23, 43, 12, 89, 2};
+    char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa"};
 
-  // Safely get the size of ages.
-  int count = sizeof(ages) / sizeof(int);
-  int i = 0;
+    // Safely get the size of ages.
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
 
-  // First way of using indexing.
-  for (i = 0; i < count; i++) {
-    printf("%s has %d years alive.\n", names[i], ages[i]);
-  }
-  printf("---\n");
+    // First way of using indexing.
+    for (i = 0; i < count; i++) {
+        printf("%s has %d years alive.\n", names[i], ages[i]);
+    }
+    printf("---\n");
 
-  // Setup the pointers to start up the program.
-  int *cur_age = ages;
-  int **cur_name = names;
+    // Set up the pointers to start up the program.
+    int *cur_age = ages;
+    char **cur_name = names;
 
-  // Second way of using pointers.
-  for (i = 0; i < count; i++) {
-    printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
-  }
-  printf("---\n");
+    // Second way of using pointers.
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
+    }
+    printf("---\n");
 
-  // Third way, pointers are just arrays.
-  for (i = 0; i < count; i++) {
-    printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
-  }
-  printf("---\n");
+    // Third way, pointers are just arrays.
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
+    }
+    printf("---\n");
 
-  // Fourth way of using pointers.
-  for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++) {
-    printf("%s lived %d years so far.\n", *cur_name, *cur_age);
-  }
+    // Fourth way of using pointers.
+    for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++) {
+        printf("%s lived %d years so far.\n", *cur_name, *cur_age);
+    }
+    printf("---\n");
 
-  return 0;
+    // 3 pointer element array.
+      int *ptr[3], iA[]={5, 10, 15}, iB[] = {1, 2, 3}, iC[] = {2, 4, 6};
+      ptr[0] = iA, ptr[1] = iB, ptr[2] = iC;
+
+    for (i = 0; i < 3; i++) {
+        printf("iA[%d]: addr:%p, data:%d\n", i, ptr[0]+1, *(ptr[0]+1));
+        printf("iB[%d]: addr:%p, data:%d\n", i, ptr[1]+1, *(ptr[1]+1));
+        printf("iC[%d]: addr:%p, data:%d\n", i, ptr[2]+1, *(ptr[2]+1));
+    }
+    return 0;
 }
